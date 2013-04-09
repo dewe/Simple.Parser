3a6a1916e36c7fda285461fa55fb309d3196e6e8
Johan Dewe <johan.dewe@fritidsresor.se>
2013-04-09 17:15:54 +0200

    Headings, list

---
+++ ParserTests/BlockElementsTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParserTests
+{
+    public class TestBase
+    {
+        protected ScrewturnToMarkupParser s2md = new ScrewturnToMarkupParser();
+    }
+
+    [TestClass]
+    public class Headings : TestBase
+    {
+        [TestMethod]
+        public void Heading1()
+        {
+            var input = "==Header 1==";
+            var expected = "# Header 1";
+
+            var actual = s2md.Transform(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Heading2()
+        {
+            var input = "===Header 2===";
+            var expected = "## Header 2";
+
+            var actual = s2md.Transform(input);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Heading3()
+        {
+            var input = "====Header 3====";
+            var expected = "### Header 3";
+
+            var actual = s2md.Transform(input);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Heading4()
+        {
+            var input = "=====Header 4=====";
+            var expected = "#### Header 4";
+
+            var actual = s2md.Transform(input);
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass]
+    public class Lists : TestBase
+    {
+        [TestMethod]
+        public void UnorderedList()
+        {
+            var input = "* List item\n* List item";
+            var expected = "* List item\n* List item";
+
+            var actual = s2md.Transform(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OrderedList()
+        {
+            var input = "# List item\n# List item";
+            var expected = "1. List item\n2. List item";
+
+            var actual = s2md.Transform(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
---
+++ ParserTests/ScrewturnToMarkupParser.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ParserTests
+{
+    public class ScrewturnToMarkupParser
+    {
+
+        public string Transform(string input)
+        {
+            var text = input;
+
+            text = FixHeaders(text);
+            text = FixLists(text);
+
+            return text;
+        }
+
+        private string FixLists(string input)
+        {
+//            var olist = new Regex(@"(?<=(\n|^))((\*|\#)+(\ )?.+?\n)+((?=\n)|\z)", RegexOptions.Compiled | RegexOptions.Singleline);
+            var olist = new Regex(@"^(\*|\#)+(\ )?(.+)?\n", RegexOptions.Compiled | RegexOptions.Singleline);
+
+            //            text = olist.Replace(text, match =>
+            return olist.Replace(input, new MatchEvaluator(
+                        (m) =>
+                        {
+                            var item = m.Groups[0];
+                            return "1. " + item;
+                        }));
+        }
+
+        private string FixHeaders(string input)
+        {
+            var h1 = new Regex(@"^==(.+?)==\n?", RegexOptions.Compiled | RegexOptions.Multiline);
+            var h2 = new Regex(@"^===(.+?)===\n?", RegexOptions.Compiled | RegexOptions.Multiline);
+            var h3 = new Regex(@"^====(.+?)====\n?", RegexOptions.Compiled | RegexOptions.Multiline);
+            var h4 = new Regex(@"^=====(.+?)=====\n?", RegexOptions.Compiled | RegexOptions.Multiline);
+
+            var text = input;
+
+            text = h4.Replace(text, new MatchEvaluator(
+                (m) =>
+                {
+                    var header = m.Groups[1].ToString();
+                    return "#### " + header;
+                }));
+
+            text = h3.Replace(text, new MatchEvaluator(
+                (m) =>
+                {
+                    var header = m.Groups[1].ToString();
+                    return "### " + header;
+                }));
+
+            text = h2.Replace(text, new MatchEvaluator(
+                (m) =>
+                {
+                    var header = m.Groups[1].ToString();
+                    return "## " + header;
+                }));
+
+            text = h1.Replace(text, new MatchEvaluator(
+                (m) =>
+                {
+                    var header = m.Groups[1].ToString();
+                    return "# " + header;
+                }));
+
+
+            return text;
+        }
+
+
+    }
+}
